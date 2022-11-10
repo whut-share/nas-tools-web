b94a824f7c1bb7bb1c4707f5a96ec150781556d3
jxxghp <jxxghp@163.com>
2022-11-10 12:37:07 +0800

    login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
+import logo from '@/assets/logo.png'
 import { useTheme } from 'vuetify'
-import logo from '@/assets/logo.svg?raw'
-import AuthProvider from '@/views/pages/authentication/AuthProvider.vue'
 
 import authV1MaskDark from '@/assets/images/pages/auth-v1-mask-dark.png'
 import authV1MaskLight from '@/assets/images/pages/auth-v1-mask-light.png'
@@ -32,24 +31,9 @@ const isPasswordVisible = ref(false)
     >
       <VCardItem class="justify-center">
         <template #prepend>
-          <div class="d-flex">
-            <div v-html="logo" />
-          </div>
+          <VImg :src="logo" :cover="true" width="9rem"/>
         </template>
-
-        <VCardTitle class="font-weight-semibold text-2xl text-uppercase">
-          Materio
-        </VCardTitle>
       </VCardItem>
-
-      <VCardText class="pt-2">
-        <h5 class="text-h5 font-weight-semibold mb-1">
-          Welcome to Materio! 👋🏻
-        </h5>
-        <p class="mb-0">
-          Please sign-in to your account and start the adventure
-        </p>
-      </VCardText>
 
       <VCardText>
         <VForm @submit.prevent="() => {}">
@@ -58,8 +42,7 @@ const isPasswordVisible = ref(false)
             <VCol cols="12">
               <VTextField
                 v-model="form.email"
-                label="Email"
-                type="email"
+                label="用户名"
               />
             </VCol>
 
@@ -67,7 +50,7 @@ const isPasswordVisible = ref(false)
             <VCol cols="12">
               <VTextField
                 v-model="form.password"
-                label="Password"
+                label="密码"
                 :type="isPasswordVisible ? 'text' : 'password'"
                 :append-inner-icon="isPasswordVisible ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
                 @click:append-inner="isPasswordVisible = !isPasswordVisible"
@@ -77,15 +60,8 @@ const isPasswordVisible = ref(false)
               <div class="d-flex align-center justify-space-between flex-wrap mt-1 mb-4">
                 <VCheckbox
                   v-model="form.remember"
-                  label="Remember me"
+                  label="保持登录"
                 />
-
-                <a
-                  class="ms-2 mb-1"
-                  href="javascript:void(0)"
-                >
-                  Forgot Password?
-                </a>
               </div>
 
               <!-- login button -->
@@ -94,7 +70,7 @@ const isPasswordVisible = ref(false)
                 type="submit"
                 to="/"
               >
-                Login
+                登录
               </VBtn>
             </VCol>
 
@@ -103,30 +79,6 @@ const isPasswordVisible = ref(false)
               cols="12"
               class="text-center text-base"
             >
-              <span>New on our platform?</span>
-              <RouterLink
-                class="text-primary ms-2"
-                :to="{ name: 'register' }"
-              >
-                Create an account
-              </RouterLink>
-            </VCol>
-
-            <VCol
-              cols="12"
-              class="d-flex align-center"
-            >
-              <VDivider />
-              <span class="mx-4">or</span>
-              <VDivider />
-            </VCol>
-
-            <!-- auth providers -->
-            <VCol
-              cols="12"
-              class="text-center"
-            >
-              <AuthProvider />
             </VCol>
           </VRow>
         </VForm>
